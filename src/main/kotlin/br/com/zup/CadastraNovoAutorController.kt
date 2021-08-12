fa67494b0da254034f85f76b2bf7b18f6838281f
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-08-11 22:25:35 -0300

    Add Header response and No-args plugin

--- src/main/kotlin/br/com/zup/CadastraNovoAutorController.kt
+++ src/main/kotlin/br/com/zup/CadastraNovoAutorController.kt
@@ -1,8 +1,11 @@
 package br.com.zup
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.validation.Valid
 
@@ -11,10 +14,24 @@ import javax.validation.Valid
 class CadastraNovoAutorController(val autorRepository: AutorRepository) {
 
     @Post
-    fun cadastra (@Body @Valid request: NovoAutorRequest) {
+    fun cadastra (@Body @Valid request: NovoAutorRequest): HttpResponse<Any> {
         println("Request => $request")
         val autor = request.toModel()
         println("Model => ${autor.nome}")
         autorRepository.save(autor)
+
+        val uri = UriBuilder.of("/autor/{id}")
+            .expand(mutableMapOf(Pair("id", autor.id)))
+
+        return HttpResponse.created(uri)
+    }
+
+    @Get
+    fun lista(): HttpResponse<List<DetalhesAutoresDto>> {
+        val autores = autorRepository.findAll()
+
+        val resposta = autores.map { autor -> DetalhesAutoresDto(autor) }
+
+        return HttpResponse.ok(resposta)
     }
 }
